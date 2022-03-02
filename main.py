113e98680f51b77098927d317fb0b096e179f9e6
Aqidah <aqidah29@hotmail.sg>
2022-03-03 01:12:40 +0800

    scrapy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import scrapy
 
 url = "https://brickset.com/sets/year-2011"
 
@@ -24,3 +25,22 @@ headers = {
 url2 = 'http://httpbin.org/headers'
 rh = requests.get(url2, headers=headers)
 print(rh.text)
+
+class NewSpider(scrapy.Spider):
+   name = "new_spider"
+   start_urls = ['http://brickset.com/sets/year-2011']
+   def parse(self, response):
+      xpath_selector = '//img'
+      for x in response.xpath(xpath_selector):
+         newsel = '@src'
+         yield {
+            'Image Link': x.xpath(newsel).extract_first(),
+         }
+
+      Page_selector = '.next a ::attr(href)'
+      next_page = response.css(Page_selector).extract_first()
+      if next_page:
+         yield scrapy.Request(
+            response.urljoin(next_page),
+            callback=self.parse
+      )
