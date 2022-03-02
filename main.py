8f38c3bd94e468cbe4227d9ec9584bcf1c46da14
Fidia <Fiyafeeyaa@gmail.com>
2022-03-03 00:54:58 +0800

    Project

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+
+url = "https://brickset.com/sets/year-2011"
+
+r = requests.get(url)
+print("Status code:")
+print("\t *", r.status_code)
+
+h = requests.head(url)
+print(url)
+print("Website Header:")
+print("****")
+
+
+
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("****")
+
+
+headers = {
+    'User-Agent' : "Mobile"
+}
+
+url2 = 'http://httpbin.org/headers'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
